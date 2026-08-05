e7194cff1bf8f5ce9cf5da9ba4755ab38cc9ef86
HuntOrBeHunted83 <learn.ishank.ramnath@gmail.com>
2026-08-05 12:16:18 -0500

    Changed the data structure

--- background.js
+++ background.js
@@ -1,57 +1,86 @@
 let currentURL;
 let url;
 
+
 // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === "complete" && tab.url && tab.url.startsWith("http")) {
-//     checkWaybackMachine(tab.url, tabId);
-//   }
+//   window.open
 // });
+
+async function setStorageLocal(items) {
+  try {
+    await chrome.storage.local.set(items)
+  } catch (error) {
+    console.error("Failed to save data to local storage:", error, chrome.runtime.lastError);
+  }
+}
+
+async function getStorageLocal(key) {
+  try {
+    let values = await chrome.storage.local.get(key)
+    // Check if the key exists in the object, even if the value is false, 0, or ""
+    if (values && key in values) {
+      return values[key];
+    }
+    return null
+  } catch (error) {
+    console.error("Failed to get data from local storage:", error, chrome.runtime.lastError);
+    return null
+  }
+}
+
+async function removeStorageLocl(items) {
+  try {
+    await chrome.storage.local.remove(items)
+  } catch (error) {
+    console.error("Failed to remove data to local storage:", error);
+  }
+}
 
 async function checkWaybackMachine(url) {
   const apiUrl =
     `https://web.archive.org/cdx/search/cdx?url=${encodeURIComponent(url)}` +
     `&output=json&fl=timestamp,original,statuscode,mimetype,digest` +
-    `&collapse=timestamp:4`;
+    `&collapse=timestamp:4` + `&from=2020&to=2026`;
 
   console.log(`Checking Wayback CDX API for: ${url}`);
 
-  // try {
-  const response = await fetch(apiUrl);
+  try {
+    const response = await fetch(apiUrl);
 
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("raw data:", data);
+    const timestamps = [];
+
+    for (let i = 1; i < data.length; i = i + 1) {
+      timestamps.push(data[i][0]);
+    }
+
+    return timestamps;
+
+    //   if (!Array.isArray(data) || data.length <= 1) {
+    //     console.log(` No snapshots found for this URL.`);
+    //     return;
+    //   }
+
+    //   const headers = data[0];
+    //   const snapshots = data.slice(1).map(row =>
+    //     Object.fromEntries(headers.map((h, i) => [h, row[i]]))
+    //   );
+
+    //   console.log(` Found ${snapshots.length} yearly snapshots:`);
+    //   snapshots.forEach((snap, index) => {
+    //     console.log(
+    //       `[${index + 1}] ${snap.timestamp} | ${snap.original} | ${snap.statuscode} | ${snap.mimetype} | ${snap.digest}`
+    //     );
+    //   });
+    //   console.log("RAW SNAPSHOT DATA", snapshots)
+  } catch (error) {
+    console.error(`API Error:`, error);
   }
-
-  const data = await response.json();
-  console.log("raw data:", data);
-  const timestamps = [];
-
-  for (let i = 1; i < data.length; i = i + 1) {
-    timestamps.push(data[i][0]);
-  }
-
-  return timestamps;
-
-  //   if (!Array.isArray(data) || data.length <= 1) {
-  //     console.log(` No snapshots found for this URL.`);
-  //     return;
-  //   }
-
-  //   const headers = data[0];
-  //   const snapshots = data.slice(1).map(row =>
-  //     Object.fromEntries(headers.map((h, i) => [h, row[i]]))
-  //   );
-
-  //   console.log(` Found ${snapshots.length} yearly snapshots:`);
-  //   snapshots.forEach((snap, index) => {
-  //     console.log(
-  //       `[${index + 1}] ${snap.timestamp} | ${snap.original} | ${snap.statuscode} | ${snap.mimetype} | ${snap.digest}`
-  //     );
-  //   });
-  //   console.log("RAW SNAPSHOT DATA", snapshots)
-  // } catch (error) {
-  //   console.error(`API Error:`, error);
-  // }
 }
 
 async function getCurrentTabUrl() {
@@ -89,24 +118,32 @@ async function changeTabURl(tabID, newUrl) {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "GET_URL") {
     (async () => {
-
-      let data = {}
+      let data = {
+        identifiers: {
+          tabURL: "",
+          tabID: "",
+        },
+        information: {}
+      }
 
       const currentURL = await getCurrentTabUrl();
+      data.identifiers["tabURL"] = currentURL
+      const tabID = await getCurrentTabId()
+      data.identifiers["tabID"] = tabID
       const timestamps = await checkWaybackMachine(currentURL);
 
       for (let i = 0; i < timestamps.length; i++) {
         const timestamp = timestamps[i];
-        const year = timestamp.slice(0, 4);
         const link = `https://web.archive.org/web/${timestamp}/${currentURL}`
 
-        data[year] = link
-        console.log("YEARS: ", year);
+        data.information[timestamp] = link
+        console.log("timestamp: ", timestamp);
         console.log("API LINKS", link);
       }
 
       console.log("DATA ", data);
 
+      await setStorageLocal({ [tabID]: data });
       sendResponse({
         data
       });
--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
     const activate = document.getElementById("rewind");
     const container = document.getElementById("years");
 
@@ -6,43 +8,43 @@ document.addEventListener("DOMContentLoaded", () => {
         chrome.runtime.sendMessage({ type: "GET_URL" }, (response) => {
             const data = response.data;
             console.log(data);
-
-            container.innerHTML = "";
-
-            for (const [year, link] of Object.entries(data)) {
-                const label = document.createElement("label");
-                const radio = document.createElement("input");
-
-                radio.type = "radio";
-                radio.name = "years";
-                radio.value = link;
-
-                label.appendChild(radio);
-                label.appendChild(document.createTextNode(year));
-                container.appendChild(label);
-
-            }
-            // years.forEach((year) => {
-            //     const label = document.createElement("label");
-            //     const radio = document.createElement("input");
-
-            //     radio.type = "radio";
-            //     radio.name = "years";
-            //     radio.value = 8278;
-
-            //     label.appendChild(radio);
-            //     label.appendChild(document.createTextNode(year));
-            //     container.appendChild(label);
-            // });
+            setRadioButtons(data, container);
         });
     });
-
 
     container.addEventListener("change", (event) => {
         if (event.target.matches('input[type="radio"][name="years"]')) {
             const selectedLink = event.target.value;
-            chrome.runtime.sendMessage({ type: "REWIND_PAGE", selectedLink: selectedLink })
-            console.log("selectedLINK", selectedLink)
+            chrome.runtime.sendMessage({ type: "REWIND_PAGE", selectedLink });
+            console.log("selectedLINK", selectedLink);
         }
     });
-});
+}
+
+function setRadioButtons(data, container) {
+    container.innerHTML = "";
+
+    for (const [year, link] of Object.entries(data)) {
+        const label = document.createElement("label");
+        const radio = document.createElement("input");
+
+        radio.type = "radio";
+        radio.name = "years";
+        radio.value = link;
+
+        label.appendChild(radio);
+        label.appendChild(document.createTextNode(year));
+        container.appendChild(label);
+    }
+}
+
+function getOldData() {
+    chrome.runtime.sendMessage({ type: "GET_OLD_DATA" }, (response) => {
+        const data = response.data;
+        console.log(data);
+        setRadioButtons(data, container);
+    });
+
+
+
+}
